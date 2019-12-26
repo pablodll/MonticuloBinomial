c0eab8527658cb57b0a51969daee970906666564
Pablo <pablo.daurell@gmail.com>
2019-12-26 01:35:11 +0100

    Pruebas de borrar minimo

--- Main.cpp
+++ Main.cpp
@@ -15,18 +15,23 @@ void mostrar(Nodo<int>* x) {
 int main() {
 	Nodo<int>* x1 = new Nodo<int>(2);
 	Nodo<int>* x2 = new Nodo<int>(3);
-	MonticuloBinomial<int>* m1 = new MonticuloBinomial<int>(x1);
+	MonticuloBinomial<int>* m1 = new MonticuloBinomial<int>();
 	MonticuloBinomial<int>* m2 = new MonticuloBinomial<int>(x2);
 
-	mostrar(m1->cabeza());
-	mostrar(m1->minimo());
+	//mostrar(m1->cabeza());
+	//mostrar(m1->minimo());
 	//mostrar(m2->minimo());
 
+	
 	m1->insertar(1);
 	mostrar(m1->cabeza());
 	mostrar(m1->minimo());
 
 	m1->insertar(3);
+	mostrar(m1->cabeza());
+	mostrar(m1->minimo());
+
+	cout << "BORRADO: " << m1->borraMin() << endl << endl;
 	mostrar(m1->cabeza());
 	mostrar(m1->minimo());
 
